71eb31921f82ca4c3141d9cff51e0f5fa4121eb4
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-01 18:23:21 -0400

    commit tree 8e71904e657ed481e1c64704d5d1be7b0a40128f

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let message = ''
+
+function scuberGreetingForFeet(distance){
+  if (distance < 400) {
+    message = 'This one is on me!';
+  } else if (2000 < distance && distance < 2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    message = 'No can do.';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch(tipAmount) {
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.';
+  }
+  return message;
 }
